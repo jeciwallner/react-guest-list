39e85f86300fc5bbcc88a629691f8d2ddeba315c
JeciHume <jecihume@gmail.com>
2021-09-29 17:41:23 +0200

    Code most features

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import logo from './images/pumpkin.png';
 
-const newGuest = [
-  { firstName: 'Johanna', lastName: 'Hume', coming: true },
-  { firstName: 'Verena', lastName: 'Jungk', coming: true },
-  { firstName: 'Michael', lastName: 'Wallner', coming: true },
-];
+// const newGuest = [
+//   { firstName: 'Johanna', lastName: 'Hume', coming: true },
+//   { firstName: 'Verena', lastName: 'Jungk', coming: true },
+//   { firstName: 'Michael', lastName: 'Wallner', coming: true },
+// ];
 
 // export default App;
 
-export default function App(props) {
+export default function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
   // create a state with a hook:
-  const [guestList, setGuestList] = useState(newGuest);
+  const [guestList, setGuestList] = useState([]);
 
   const handleChangeFirst = (event) => {
     const updatedFirstName = event.target.value;
@@ -27,15 +27,15 @@ export default function App(props) {
     const updatedLastName = e.target.value;
     setLastName(updatedLastName);
   };
-  const [coming, setComing] = useState('');
-  const initialList = [];
+  // const [coming, setComing] = useState(false);
 
   const fetchGuests = async () => {
-    const res = await axios.get(
-      'https://github.com/upleveled/express-guest-list-api-memory-data-store',
-    );
-    setGuestList(res.data);
+    const response = await fetch('https://pumpkinpieshalloween.herokuapp.com');
+    const allGuests = await response.json();
+    setGuestList(allGuests);
+    console.log(allGuests);
   };
+  // use Effect makes sure that anything that's inside gets reloaded when the page reloads
   useEffect(() => {
     fetchGuests();
   }, []);
@@ -49,12 +49,7 @@ export default function App(props) {
         <br></br>
         <br></br>
         <br></br>
-        <img
-          src={logo}
-          className="
-        App-logo"
-          alt="logo"
-        />
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
 
       <div className="App-form">
@@ -68,7 +63,7 @@ export default function App(props) {
         </label>
         <br></br>
         {/* question with checkbox, checking attendance: */}
-        <label>
+        {/* <label>
           Are you coming?
           <input
             type="checkbox"
@@ -77,19 +72,39 @@ export default function App(props) {
             // 3. Change the state variable when the user interacts
             onChange={(event) => setComing(event.currentTarget.checked)}
           />
-        </label>
+        </label> */}
         {/* Answers to whether the person is attending: */}
-        <div>
+        {/* <div>
           Your answer:{' '}
           {coming ? 'See you there!' : 'Sorry, I`m hanging someplace else.'}
-        </div>
+        </div> */}
 
-        <br></br>
+        <br />
 
         <button
           onClick={() => {
-            guestList.push({});
-            setGuestList([newGuest[(0, 2)]]);
+            async function postGuest() {
+              // language is json
+              const response = await fetch(
+                `https://pumpkinpieshalloween.herokuapp.com`,
+                {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                  }),
+                },
+              );
+              const createdGuest = await response.json();
+              console.log(createdGuest);
+              fetchGuests();
+            }
+            postGuest();
+            setFirstName('');
+            setLastName('');
           }}
         >
           Hit it!
@@ -99,12 +114,56 @@ export default function App(props) {
       <ul>
         {guestList.map((newGuest) => {
           return (
-            <li
-              key={newGuest.firstName}
-              key={newGuest.lastName}
-              key={newGuest.coming}
-            >
+            <li key={newGuest.id}>
               {newGuest.firstName} {newGuest.lastName}
+              {newGuest.attending
+                ? ' is going to bring the house down!'
+                : ' is hanging someplace else.'}
+              <button
+                onClick={() => {
+                  async function deleteGuest() {
+                    const response = await fetch(
+                      `https://pumpkinpieshalloween.herokuapp.com/${newGuest.id}`,
+                      {
+                        method: 'DELETE',
+                      },
+                    );
+                    const deletedGuest = await response.json();
+                    console.log(
+                      `https://pumpkinpieshalloween.herokuapp.com/${newGuest.id}`,
+                      deletedGuest,
+                    );
+                    fetchGuests();
+                  }
+                  deleteGuest();
+                }}
+              >
+                delete
+              </button>
+              <button
+                onClick={() => {
+                  async function updateGuest() {
+                    const response = await fetch(
+                      `https://pumpkinpieshalloween.herokuapp.com/${newGuest.id}`,
+                      {
+                        method: 'PATCH',
+                        headers: {
+                          'Content-Type': 'application/json',
+                        },
+                        // in this case ! means : do the opposite with booleans / do not what's in the variable
+                        body: JSON.stringify({
+                          attending: !newGuest.attending,
+                        }),
+                      },
+                    );
+                    const updatedGuest = await response.json();
+                    fetchGuests();
+                  }
+                  updateGuest();
+                }}
+              >
+                Are You Coming?
+              </button>
             </li>
           );
         })}
